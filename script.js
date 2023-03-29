85d679ec3859b358481a3a04d3b1aea5af77494c
Talha Rajpoot <rt3830279@gmail.com>
2023-03-29 15:18:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+//  
+//                                 program No.1
+//                  Write a js program to find maximum between two numbers. 
+
+// var num1 =+ prompt("Enter tha 1st Number")
+// var num2 =+ prompt("Enter tha 2nd Number")
+
+// if (num1 > num2) {
+//     console.log("num1 is maximum")
+// }
+
+// else{
+//     console.log("num1 is not maximum")
+// }
+
+
+//                             --------------------------------------------
+
+    
+//                                            program No.2
+//                     Write a js program to find maximum between three numbers.
+
+// var num1 =+prompt("Enter tha 1st Number")
+// var num2 =+prompt("Enter tha 2nd Number")
+// var num3 =+prompt("Enter tha 3rd Number")
+
+//  if (num1 > num2 & num1 > num3) {
+//     console.log("num1 is maximum")
+//  }
+
+//  else if ( num2 > num1 & num2 > num3) {
+//     console.log("num2 is maximum")
+//  }
+ 
+//  else if (num3 > num1 & num3 > num2) {
+//     console.log("num3 is maximum")
+//  }
+ 
+
+//                          ----------------------------------------------
+
+//                                            program No.3
+//               Write a js program to check whether a number is negative, positive or zero
+
+
+// var num1 =+ prompt("Enter tha Number")
+
+
+// if (num1 < 0) {
+//     console.log("This is a Negative Number")
+// }
+
+// else if (num1 > 0) {
+//     console.log("This is a Positive Number")
+// }
+
+// else if (num1 == 0) {
+//     console.log("This is a Zero Number")
+// }
+
+
+
+//                             ------------------------------------------------
+
+ //                                              program Node.4
+ //                   Write a js program to check whether a number is divisible by 5 and 11 or not
+
+
+// var num1 =+ prompt ("Enter tha Number")
+
+
+// if (num1%5 == 0) {
+//     console.log("this number is divisible by 5")
+// }
+
+// else if (num1%11 == 0) {
+//     console.log("this number is divisible by 11")
+// }
+
+// else {
+//     console.log("This number is not divisible by 11 or 5")
+// }
+
+
+//                            -------------------------------------------------
+
+
+//                                              program no.5
+//                      Write a js program to check whether a number is even or odd.
+
+
+// var num1 =+ prompt("Enter tha Number")
+
+
+// if (num1%2 == 0) {
+//     console.log("This is an Even Number")
+// }
+
+// else {
+//     console.log("This number is an odd Number")
+// }
+
+
+//                           -----------------------------------------------------
+
+
+//                                               program No.5
+//                       Write a js program to check whether a year is leap year or not.
